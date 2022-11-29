7eccf5d88f8170e62c95cf5bd0cf9ce04718463c
gabrielMatosBoubee <gm.boubee@gmail.com>
2022-11-29 16:03:26 -0300

    adicionei o redux e configurei ele a aplicação

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import store from './redux';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Provider store={ store }>
+      <div className="meals">
+        <span className="logo">TRYBE</span>
+        <object
+          className="rocksGlass"
+          type="image/svg+xml"
+          data={ rockGlass }
+        >
+          Glass
+        </object>
+      </div>
+    </Provider>
   );
 }
 
