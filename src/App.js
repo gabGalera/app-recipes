4650be72813e003573bfb8dadbdb76e551276e51
Gabriel Galera <gabgalera@hotmail.com>
2022-11-29 17:21:35 -0300

    COloca o Login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
-import rockGlass from './images/rockGlass.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login';
 import store from './redux';
 
 function App() {
   return (
     <Provider store={ store }>
-      <div className="meals">
-        <span className="logo">TRYBE</span>
-        <object
-          className="rocksGlass"
-          type="image/svg+xml"
-          data={ rockGlass }
-        >
-          Glass
-        </object>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </BrowserRouter>
     </Provider>
   );
 }
