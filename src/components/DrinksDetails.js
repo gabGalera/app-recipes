ca8c6f44e663d57699460a6fc579e937f79a2a18
Gabriel Galera <gabgalera@hotmail.com>
2022-12-01 17:14:07 -0300

    Divide o componente em dois e cria a page

---
+++ src/components/DrinksDetails.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+function DrinksDetails({
+  API, renderIngredients, recommendations, doneRecipes, id, inProgressRecipes }) {
+  return (
+    <div>
+      <button
+        type="button"
+        data-testid="share-btn"
+      >
+        Share Recipe
+      </button>
+      <button
+        type="button"
+        data-testid="favorite-btn"
+      >
+        Favorite
+      </button>
+      <img
+        src={ API[0].strDrinkThumb }
+        alt={ API[0].strDrink }
+        data-testid="recipe-photo"
+      />
+      <p data-testid="recipe-title">{API[0].strDrink}</p>
+      <p data-testid="recipe-category">{API[0].strAlcoholic}</p>
+      {renderIngredients()}
+      <p data-testid="instructions">{API[0].strInstructions}</p>
+      <p data-testid="video">{API[0].strVideo}</p>
+      <h1>Recommended</h1>
+      <div
+        style={ {
+          display: 'flex',
+          width: '100%',
+          overflowX: 'scroll',
+        } }
+      >
+        {recommendations
+          .filter((value, index) => {
+            const maxRecommendations = 6;
+            return index < maxRecommendations;
+          })
+          .map((entry, index) => (
+            <div key={ index } data-testid={ `${index}-recommendation-card` }>
+              <p data-testid={ `${index}-recommendation-title` }>
+                {
+                  entry.strMeal
+                }
+              </p>
+              <img
+                src={ entry.strMealThumb }
+                alt={ entry.strMeal }
+                style={ {
+                  padding: '1vw',
+                  width: '180px',
+                } }
+              />
+            </div>
+          ))}
+      </div>
+      {!doneRecipes.some((entry) => entry.id === API[0].idDrink) && (
+        <Link to={ `./${id}/in-progress` }>
+          <button
+            type="button"
+            data-testid="start-recipe-btn"
+            style={ {
+              position: 'fixed',
+              bottom: '0px',
+            } }
+          >
+            Start Recipe
+          </button>
+        </Link>
+      )}
+      {Object.keys(inProgressRecipes.drinks)
+        .some((entry) => entry === API[0].idDrink)
+          && (
+            <button
+              type="button"
+              data-testid="start-recipe-btn"
+              style={ {
+                position: 'fixed',
+                bottom: '0px',
+              } }
+            >
+              Continue Recipe
+            </button>
+          )}
+    </div>
+  );
+}
+
+DrinksDetails.propTypes = ({
+  API: PropTypes.arrayOf().isRequired,
+  renderIngredients: PropTypes.func.isRequired,
+  recommendations: PropTypes.arrayOf().isRequired,
+  doneRecipes: PropTypes.arrayOf().isRequired,
+  id: PropTypes.string.isRequired,
+  inProgressRecipes: PropTypes.shape().isRequired,
+});
+
+export default DrinksDetails;
---
+++ src/components/MealsDetails.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+function MealsDetails({
+  API, renderIngredients, recommendations, doneRecipes, id, inProgressRecipes }) {
+  return (
+    <div>
+      <button
+        type="button"
+        data-testid="share-btn"
+      >
+        Share Recipe
+      </button>
+      <button
+        type="button"
+        data-testid="favorite-btn"
+      >
+        Favorite
+      </button>
+      <img
+        src={ API[0].strMealThumb }
+        alt={ API[0].strMeal }
+        data-testid="recipe-photo"
+      />
+      <p data-testid="recipe-title">{API[0].strMeal}</p>
+      <p data-testid="recipe-category">
+        {API[0].strCategory}
+      </p>
+      {renderIngredients()}
+      <p data-testid="instructions">{API[0].strInstructions}</p>
+      <p data-testid="video">{API[0].strVideo}</p>
+      <h1>Recommended</h1>
+      <div
+        style={ {
+          display: 'flex',
+          width: '100%',
+          overflowX: 'scroll',
+        } }
+      >
+        {recommendations
+          .filter((value, index) => {
+            const maxRecommendations = 6;
+            return index < maxRecommendations;
+          })
+          .map((entry, index) => (
+            <div key={ index } data-testid={ `${index}-recommendation-card` }>
+              <p data-testid={ `${index}-recommendation-title` }>
+                {
+                  entry.strDrink
+                }
+              </p>
+              <img
+                src={ entry.strDrinkThumb }
+                alt={ entry.strDrink }
+                style={ {
+                  padding: '1vw',
+                  width: '180px',
+                } }
+              />
+            </div>
+          ))}
+      </div>
+      {!doneRecipes.some((entry) => entry.id === API[0].idMeal) && (
+        <Link to={ `./${id}/in-progress` }>
+          <button
+            type="button"
+            data-testid="start-recipe-btn"
+            style={ {
+              position: 'fixed',
+              bottom: '0px',
+            } }
+          >
+            Start Recipe
+          </button>
+        </Link>
+      )}
+      {Object.keys(inProgressRecipes.meals)
+        .some((entry) => entry === API[0].idMeal)
+              && (
+                <button
+                  type="button"
+                  data-testid="start-recipe-btn"
+                  style={ {
+                    position: 'fixed',
+                    bottom: '0px',
+                  } }
+                >
+                  Continue Recipe
+                </button>
+              )}
+    </div>
+  );
+}
+
+MealsDetails.propTypes = ({
+  API: PropTypes.arrayOf().isRequired,
+  renderIngredients: PropTypes.func.isRequired,
+  recommendations: PropTypes.arrayOf().isRequired,
+  doneRecipes: PropTypes.arrayOf().isRequired,
+  id: PropTypes.string.isRequired,
+  inProgressRecipes: PropTypes.shape().isRequired,
+});
+
+export default MealsDetails;
