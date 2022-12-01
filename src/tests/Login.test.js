978431e56fcc51cadcf8c3dd545a2067f5901898
Guilherme Crupi <guilhermecrupi@hotmail.com>
2022-12-01 18:13:07 -0300

    Alterando posição do BrowserRouter e do Provider, alterando função renderWithRouterAndRedux

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,7 +1,7 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { renderWithRouter } from './renderWith';
-import App from '../pages/Login';
+import App from '../App';
+import renderWithRouterAndRedux from './renderWithRouterAndRedux';
 
 describe('Testes para pagina Login', () => {
   const dataTestIdEmail = 'email-input';
@@ -9,19 +9,19 @@ describe('Testes para pagina Login', () => {
   const emailTest = 'test@test.com';
 
   test('testa se a pagina Login esta na rota /', () => {
-    const { history } = renderWithRouter(<App />);
+    const { history } = renderWithRouterAndRedux(<App />);
     const { pathname } = history.location;
     expect(pathname).toEqual('/');
   });
   test('testa se a pagina contem os inputs de Email e Senha', () => {
-    renderWithRouter(<App />);
+    renderWithRouterAndRedux(<App />);
     const emailInput = screen.getByTestId(dataTestIdEmail);
     const passowordInput = screen.getByTestId(dataTestIdPassword);
     expect(passowordInput).toBeInTheDocument();
     expect(emailInput).toBeInTheDocument();
   });
   test('testa se o botão esta desativado caso a validação de email não seja correspondente', () => {
-    renderWithRouter(<App />);
+    renderWithRouterAndRedux(<App />);
     const inputEmail = screen.getByTestId(dataTestIdEmail);
     const inputSenha = screen.getByTestId(dataTestIdPassword);
     userEvent.type(inputEmail, 'test@a');
@@ -30,7 +30,7 @@ describe('Testes para pagina Login', () => {
     expect(loginButton).toBeDisabled();
   });
   test('testa se as validações de senha e email estão funcionando', () => {
-    const { history } = renderWithRouter(<App />);
+    const { history } = renderWithRouterAndRedux(<App />);
     const inputEmail = screen.getByTestId(dataTestIdEmail);
     const inputSenha = screen.getByTestId(dataTestIdPassword);
     const loginButton = screen.getByRole('button', { name: 'Enter' });
@@ -38,7 +38,6 @@ describe('Testes para pagina Login', () => {
     userEvent.type(inputSenha, '1234567');
     expect(loginButton).toBeEnabled();
     userEvent.click(loginButton);
-    const { pathname } = history.location;
-    expect(pathname).toEqual('/meals');
+    expect(history.location.pathname).toEqual('/meals');
   });
 });
