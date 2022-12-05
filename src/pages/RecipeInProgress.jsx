b971a08159bdf625edb7293e5ed0ade911732780
Gabriel Galera <gabgalera@hotmail.com>
2022-12-05 10:17:18 -0300

    Renderiza a página RecipeInProgress

--- src/pages/RecipeInProgress.jsx
+++ src/pages/RecipeInProgress.jsx
@@ -1,17 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-// import DetailsInformationsMeals from '../components/DetailsInformationsMeals';
+import { setAPIDetails, setRecommendationsDetails } from '../redux/actions';
+import { fecthDrinkDetails,
+  fecthMealsDetails,
+  fetchDrinksRecommendations,
+  fetchMealsRecommendations } from '../services/recipeDetails';
+import DetailsInformationsDrinks from '../components/DetailsInformationsDrinks';
+import DetailsInformationsMeals from '../components/DetailsInformationsMeals';
+import FavAndShareBtnDrinks from '../components/FavAndShareBtnDrinks';
+import FavAndShareBtnMeals from '../components/FavAndShareBtnMeals';
 
 function RecipeInProgress() {
+  const [isLoadingMain, setIsLoadingMain] = useState(true);
+  const [isLoadingRecommendation, setIsLoadingRecommendation] = useState(true);
+
+  const dispatch = useDispatch();
   const history = useHistory();
   const { location: { pathname } } = history;
+
+  const componentDidMount = async () => {
+    if (pathname.split('/')[1] === 'meals') {
+      dispatch(setRecommendationsDetails(await fetchMealsRecommendations()));
+      dispatch(setAPIDetails(await fecthMealsDetails(pathname)));
+      setIsLoadingRecommendation(false);
+      setIsLoadingMain(false);
+    }
+    if (pathname.split('/')[1] === 'drinks') {
+      dispatch(setAPIDetails(await fecthDrinkDetails(pathname)));
+      dispatch(setRecommendationsDetails(await fetchDrinksRecommendations()));
+      setIsLoadingRecommendation(false);
+      setIsLoadingMain(false);
+    }
+  };
+
+  useEffect(() => {
+    componentDidMount();
+  }, []);
+
+  if (isLoadingMain || isLoadingRecommendation) return <h1>Loading...</h1>;
+
   if (pathname.split('/')[1] === 'meals') {
     return (
-      <h1>Hello World!</h1>
+      <>
+        <FavAndShareBtnMeals />
+        <DetailsInformationsMeals />
+        <button
+          type="button"
+          data-testid="finish-recipe-btn"
+        >
+          Finish Recipe
+        </button>
+      </>
     );
   }
   return (
-    <h1>Hello World!</h1>
+    <>
+      <FavAndShareBtnDrinks />
+      <DetailsInformationsDrinks />
+      <button
+        type="button"
+        data-testid="finish-recipe-btn"
+      >
+        Finish Recipe
+      </button>
+    </>
   );
 }
 
